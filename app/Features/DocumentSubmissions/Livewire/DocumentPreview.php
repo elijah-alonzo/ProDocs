327f3bfba6459c543bdc314373b10e80f5fbe1cb
elijah-alonzo <elijahalonzo.me@gmail.com>
2026-07-09 20:36:19 +0800

    add: livewire component for document preview

---
+++ app/Features/DocumentSubmissions/Livewire/DocumentPreview.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Features\DocumentSubmissions\Livewire;
+
+use App\Features\DocumentSubmissions\Models\DocumentSubmission;
+use Illuminate\Contracts\View\View;
+use Livewire\Component;
+
+class DocumentPreview extends Component
+{
+    public DocumentSubmission $submission;
+
+    public function render(): View
+    {
+        return view('DocumentPreview');
+    }
+}
